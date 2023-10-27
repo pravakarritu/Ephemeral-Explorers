e460d3413b06396f2aed49a1885f25535169db5f
Himanshu Rawlani <himanshurawlani@hotmail.com>
2023-10-26 23:37:47 -0700

    Added temporary tutorial page

--- Assets/Scripts/ShowMenu.cs
+++ Assets/Scripts/ShowMenu.cs
@@ -6,26 +6,26 @@ public class ShowMenu : MonoBehaviour
 {
     private GameObject ExitButton, TutorialButton, PlayerControlsButton;
     private bool isClicked = false;
-    
+
     void Start()
     {
         ExitButton = GameObject.Find("/Canvas/Exit");
-        // TutorialButton = GameObject.Find("/Canvas/Tutorial");
         PlayerControlsButton = GameObject.Find("/Canvas/PlayerControls");
         ExitButton.SetActive(false);
-        // TutorialButton.SetActive(false);
         PlayerControlsButton.SetActive(false);
     }
 
     public void MenuSet()
     {
-        if (isClicked) {
+        if (isClicked)
+        {
             ExitButton.SetActive(false);
             // TutorialButton.SetActive(false);
             PlayerControlsButton.SetActive(false);
             isClicked = false;
         }
-        else {
+        else
+        {
             ExitButton.SetActive(true);
             // TutorialButton.SetActive(true);
             PlayerControlsButton.SetActive(true);
--- Assets/Scripts/TutorialButton.cs
+++ Assets/Scripts/TutorialButton.cs
@@ -5,37 +5,46 @@ using UnityEngine;
 public class TutorialButton : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject wholeWorld;
     public GameObject tutorial;
+    private bool tutorialActive = false;
     public GameObject levels;
+    private bool levelsActive = false;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void unActiveWorld(){
-        // wholeWorld.SetActive(false);
-        tutorial.SetActive(true);
+    public void toggleTutorial()
+    {
+        if (tutorialActive)
+        {
+            tutorial.SetActive(false);
+            tutorialActive = false;
+        }
+        else
+        {
+            tutorial.SetActive(true);
+            tutorialActive = true;
+        }
     }
 
-    public void ActiveWorld(){
-        // wholeWorld.SetActive(true);
-        tutorial.SetActive(false);
+    public void toggleLevels()
+    {
+        if (levelsActive)
+        {
+            levels.SetActive(false);
+            levelsActive = false;
+        }
+        else
+        {
+            levels.SetActive(true);
+            levelsActive = true;
+        }
     }
-
-    public void ActiveLevels(){
-        levels.SetActive(true);
-    }
-
-     public void unActiveLevels(){
-        levels.SetActive(false);
-    }
-
-
 }
